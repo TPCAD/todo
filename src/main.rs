7bb158edf2816a400cbf0cb2f2d016cceb517f79
TPCAD <1814083420@qq.com>
2024-07-07 10:39:07 +0800

    refactor: remove files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-use std::env;
-
-use todo::{config::Config, misc, run};
-
-fn main() {
-    let (action, target) = misc::parse_args();
-
-    let home_dir = env::var("HOME").unwrap();
-    let home_dir = home_dir + "/.config/todo";
-    let config = Config::new(&home_dir);
-    config.create_config_file();
-
-    run(&action, &target, &config);
-}
+fn main() {}
